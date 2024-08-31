6602ab34a3694e8c9acb9815109435a973377243
Izet Delibasic <izetdelibasic55@gmail.com>
2024-08-31 09:15:32 +0200

    Adding links constant for app options

---
+++ src/constants/linksConstant.js
@@ -0,0 +1,14 @@
+export const Links = [
+  {
+    title: "Containers",
+    href: "/dashboard",
+  },
+  {
+    title: "Support",
+    href: "/support",
+  },
+  {
+    title: "Profile",
+    href: "/profile",
+  },
+];
